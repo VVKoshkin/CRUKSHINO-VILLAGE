e8a9439e6e5af9ce5c213081a35e7caa756ae0f9
VV.Koshkin <VV.Koshkin@e-soft.ru>
2023-03-10 13:35:27 +0300

    almost done...need to do smth with sliders...

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(() => {
-    // implement something here...
+    $('.pictures').slick({
+        arrows: true,
+        appendArrows: $(this).siblings('.contains-picture-slider').find('.slider'),
+        prevArrow: '<button class="slider-btn back"></button>',
+        nextArrow: '<button class="slider-btn forw"></button>',
+        dots: false,
+    });
 });
